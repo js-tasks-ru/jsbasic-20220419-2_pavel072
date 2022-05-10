c154b027f40831529cb212a456dd03106cceef16
Pavel Popov <p.popov@roseltorg.ru>
2022-05-10 10:48:28 +0300

    Решение задачи 1 и 2 модуль 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,8 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  button.addEventListener('click', function (event) {
+    let text = document.querySelector('#text');
+    if (!text) return;
+    text.hidden = !text.hidden;
+  });
 }
